ccdad5d7e173b78d1376533ef187e7e0e765c4ab
Basquiat <ememdaniel98@gmail.com>
2021-10-28 13:24:30 -0500

    got all tables and columns created and seeded database

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,16 +5,36 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+    /// add foreign key and on delete
+    foreignKey: 'product_id',
+    onDelete: 'CASCADE'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+        //// add foreign key and on delete
+        foreignKey: 'category_id',
+        // onDelete: 'CASCADE'
+    }),
 
-// Products belongToMany Tags (through ProductTag)
+    // Products belongToMany Tags (through ProductTag)
+    Product.belongsToMany(Tag, {
+        through: ProductTag,
+        foreignKey: 'product_id'
+            //add foreign key
+    });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+    foreignKey: 'tag_id'
+        //add foreign key
+})
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+    Product,
+    Category,
+    Tag,
+    ProductTag,
 };
