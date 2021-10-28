cd26089669df0db3360478741de088c6e2ad899d
Basquiat <ememdaniel98@gmail.com>
2021-10-28 18:18:38 -0500

    added read me and finished routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,26 +3,77 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/all-tags', async(req, res) => {
+    // find all tags
+    // be sure to include its associated Product data
+    try {
+        const tagData = await Tag.findAll({
+            include: [{ model: Product }],
+        });
+        res.status(200).json(tagData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/id/:id', async(req, res) => {
+    // find a single tag by its `id`
+    // be sure to include its associated Product data
+    try {
+        const tagData = await Tag.findByPk(req.params.id, {
+            include: [{ model: Product }],
+        });
+
+        if (!tagData) {
+            res.status(404).json({ message: 'No tags found!' });
+            return;
+        }
+
+        res.status(200).json(tagData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/new-tag', async(req, res) => {
+    // create a new tag
+    try {
+        const tagData = await Tag.create({
+            tag_name: req.body.tag_name,
+        });
+        res.status(200).json(tagData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/update/:id', (req, res) => {
+    // update a tag's name by its `id` value
+    Tag.update({
+
+            tag_name: req.body.tag_name,
+        }, {
+            where: {
+                id: req.params.id,
+            },
+        })
+        .then((tagData) => {
+            res.json(tagData);
+        })
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/delete/:id', (req, res) => {
+    // delete on tag by its `id` value
+    Tag.destroy({
+            where: {
+                id: req.params.id
+            },
+        })
+        .then((deletedTag) => {
+            res.json(deletedTag);
+        })
+        .catch((err) => res.json(err));
+
 });
 
 module.exports = router;
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# ecommerce
+# ECOMMERCE DATABASE
+
+## Description
+This database is intended so serve as a way to store ecommerce data, with Category sections to seperate products by their categories (ie: jeans, etc), Product sections for specific Products (ie: blue flair high rise jeans, with price, stock and category listed), and Tags to designate different attributes (ie: vintage, with the specific product id and tag ids).
+
+## Usage
+This database can not only store, but allows the user to search by category, product or tags to find specific items. These categories, products and tags can also be searched by their unique ids, updated by their unique ids, and deleted by their unique ids, and allows new categories, products and tags to be added to the database.
+
+## Links
+repository:
+https://github.com/emilyelizabethdaniel
+
+video walk-through: 
+part one: https://watch.screencastify.com/v/o8BBxuo5OgPRYquLFbgK
+part two: https://watch.screencastify.com/v/xCENQV7T0KlGJkk6biJc
+part three: https://watch.screencastify.com/v/BOFk2Z7zQ013tU8jndxk
